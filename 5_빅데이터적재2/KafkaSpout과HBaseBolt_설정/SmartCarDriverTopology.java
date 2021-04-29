d4a6b588a49428e8b49d0f6826dc0c0938a1c009
KimHyungkeun <fnfn9087@gmail.com>
2021-04-29 16:48:53 +0900

    BigData_Pilot_Pjt

--- 5_빅데이터적재2/KafkaSpout과HBaseBolt_설정/SmartCarDriverTopology.java
+++ 5_빅데이터적재2/KafkaSpout과HBaseBolt_설정/SmartCarDriverTopology.java
@@ -48,9 +48,11 @@ public class SmartCarDriverTopology {
 	}  
 
 
-
+	// 스톰 Spout의 기본 기능은 외부 시스템과의 연동을 통해 스톰의 Topology로 데이터를 가져오는 것이다.
+	// Kafka가 외부시스템과 스톰의 Topology를 이어주는 역할을 하게 된다
 	private static StormTopology makeTopology() {
 
+		// 1) KafkaSpout 객체를 생성. 주키퍼 서버 정보, 카프카 토픽 정보, 메시지 형식, 메시지 수신 방식 등을 설정
 		String zkHost = "server02.hadoop.com:2181";
 		TopologyBuilder driverCarTopologyBuilder = new TopologyBuilder();
 		
@@ -63,15 +65,21 @@ public class SmartCarDriverTopology {
 		spoutConf.scheme = new SchemeAsMultiScheme(new StringScheme());
 		KafkaSpout kafkaSpout = new KafkaSpout(spoutConf);
 		
+		// 2) KafkaSpout 객체를 스톰의 Topology에 설정. "고유ID", "kafkaSpout객체", "병렬 처리 힌트 제공 여부"
 		driverCarTopologyBuilder.setSpout("kafkaSpout", kafkaSpout, 1);
 		
 		
 		// Grouping - SplitBolt & EsperBolt
+		// 3) Kafka가 수신받은 데이터를 어떻게 라우팅할지에 대한 설정이다.
+		// 스톰 Topology의 그루핑 설정으로 All-Grouping 기능을 설정한 "kafkaSpout"로부터 받은 두 데이터를 두개의 Bolt(Split/Esper)에 동일하게 전달
 		driverCarTopologyBuilder.setBolt("splitBolt", new SplitBolt(),1).allGrouping("kafkaSpout");
 		driverCarTopologyBuilder.setBolt("esperBolt", new EsperBolt(),1).allGrouping("kafkaSpout");
 		
 		
 		// HBase Bolt
+		// 4) HBase의 데이터를 적재하기 위한 Config 정보 설정
+		// "DriverCarInfo"에 고유 테이블 번호인 "r_key"를 정의
+		// 칼럼 패밀리 cf1에 해당하는 8개의 칼럼명 정보와 Zookeeper 연결 정보도 확인
 		TupleTableConfig hTableConfig = new TupleTableConfig("DriverCarInfo", "r_key");
 		hTableConfig.setZkQuorum("server02.hadoop.com");
 		hTableConfig.setZkClientPort("2181");
@@ -85,7 +93,10 @@ public class SmartCarDriverTopology {
 		hTableConfig.addColumn("cf1", "speed");
 		hTableConfig.addColumn("cf1", "area_number");
 		
+		// 5) 설정한 HBase의 Config 정보(hTableConfig)를 이요해 HBaseBolt 객체 생성
 		HBaseBolt hbaseBolt = new HBaseBolt(hTableConfig);
+		// 6) HBaseBolt 객체를 스톰의 Topology에 설정. "고유ID", "HBaseBolt 객체", "병렬 처리 힌트"를 설정.
+		// SplitBolt로 부터 데이터를 전달받기 위해 그루핑명을 "splitBolt"로 설정. ( 3)단계에서 설정한 이름 )
 		driverCarTopologyBuilder.setBolt("HBASE", hbaseBolt, 1).shuffleGrouping("splitBolt");
 		
 		
